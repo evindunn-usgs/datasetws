ab5561975c32659a9988ef7759a98ba0a11fcce3
Scott Akins <sakins@usgs.gov>
2017-11-29 12:58:05 -0700

    Initial check in from existing SVN repository

---
+++ datasetws.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+
+from flask import Flask, abort, flash, redirect, render_template, request, url_for, Response
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from datasource import datasourcebase, Nomenclature, PGM
+# from nomenclature import Nomenclature
+from dswfs import wfs
+
+WSApp = Flask(__name__, instance_relative_config=True)
+WSApp.debug=True
+WSApp.config.from_pyfile('config.py')
+
+@WSApp.route("/")
+def indexHandler():
+  return redirect("docs/index.html")
+
+@WSApp.route("/data")
+def listDataServices():
+  # WSApp.logger.debug("listDataServices called.")
+  interfaces = {}
+  datasrc = datasourcebase.factory ('nomenclature', WSApp.config)
+  interfaces['nomenclature'] = datasrc.loadInterfaces()
+  datasrc2 = datasourcebase.factory ('pgm', WSApp.config)
+  interfaces['pgm'] = datasrc2.loadInterfaces()
+  dswfs = wfs (WSApp.config)
+  interfaces['wfs'] = dswfs.loadInterfaces()
+
+  metadata = {}
+  metadata['datasets'] = {'nomenclature':{}, 'pgm':{}}
+  metadata['protocols'] = ['WFS']
+  metadata['datasets']['nomenclature']['targets'] = interfaces['nomenclature']['targets']()
+  metadata['datasets']['nomenclature']['metadata'] = interfaces['nomenclature']['metadata']()
+  metadata['datasets']['nomenclature']['neutralsearch'] = interfaces['wfs']['neutralSearch'](request)
+  metadata['datasets']['pgm']['targets'] = interfaces['pgm']['targets']()
+  metadata['datasets']['pgm']['metadata'] = interfaces['pgm']['metadata']()
+  metadata['datasets']['pgm']['neutralsearch'] = interfaces['wfs']['neutralSearch'](request)
+  
+  return render_template('datalist.html', metadata=metadata)
+
+@WSApp.route("/data/<dataset>/summary", methods=['GET', 'POST'])
+def dataSummary(dataset):
+  interfaces = {}
+  datasrc = datasourcebase.factory (dataset, WSApp.config)
+  interfaces[dataset] = datasrc.loadInterfaces()
+  summaryresults = interfaces[dataset]['summary']()
+
+  # WSApp.logger.debug(summaryresults)
+  return Response(render_template('astro/summary.json', results=summaryresults), mimetype='application/json')
+  #return render_template('astro/summary.json', results=summaryresults)
+
+@WSApp.route("/data/<dataset>/<target>/<protocol>", methods=['GET', 'POST'])
+def dataRequest(dataset, target, protocol):
+  # WSApp.logger.debug("dataRequest called.")
+  interfaces = {}
+  datasrc = datasourcebase.factory (dataset, WSApp.config)
+  interfaces[dataset] = datasrc.loadInterfaces()
+  dswfs = wfs (WSApp.config)
+  interfaces['wfs'] = dswfs.loadInterfaces()
+
+  metadata = {"dataset":dataset, "target":target, "protocol":protocol, "request":request}
+  # try:
+  metadata['datasetmeta'] = interfaces[dataset.lower()]['metadata']()
+
+  metadata['neutralsearch'] = interfaces[protocol.lower()]['neutralSearch'](request)
+  metadata['neutralsearch']['target'] = target.upper()
+  metadata['searchresults'] = interfaces[dataset.lower()]['search'](metadata['neutralsearch'])
+  # WSApp.logger.debug(metadata['searchresults']['columns'])
+
+  return interfaces[protocol.lower()]['renderResponse'](metadata)
+
+  # except:
+  #   dump = dir(__builtins__)
+  #   WSApp.logger.debug("Exception triggered")
+  #   WSApp.logger.debug(metadata)
+  #   WSApp.logger.debug(interfaces)
+
+  #   return render_template('datasetresult.html', searchargs=metadata)
+
+if __name__ == "__main__":
+  WSApp.run(debug=True)
+
---
+++ dswfs.py
@@ -0,0 +1,96 @@
+from flask import Flask, abort, render_template, request, url_for, Response
+import re
+
+class wfs:
+  'Web Feature Service (WFS) protocol class implementation'
+
+  def __init__(self, config):
+    self.config = config
+
+  def neutralSearch(self, request):
+    neutralCriteria = {}
+    neutralCriteria['protocol'] = {}
+    neutralCriteria['criteria'] = {}
+
+    # neutralCriteria['base_url'] = request.url_root
+    neutralCriteria['base_url'] = request.url_root.replace('http://', 'https://')
+
+    request.args = {k.lower():v for k,v in request.args.items()}
+    if request.args.get('request'):
+      neutralCriteria['protocol']['request'] = request.args.get('request').lower()
+    else:
+      neutralCriteria['protocol']['request'] = ''
+    if request.args.get('outputformat'):
+      neutralCriteria['protocol']['outputformat'] = request.args.get('outputformat').lower()
+    else:
+      neutralCriteria['protocol']['outputformat'] = ''
+
+    # The following are required parameters, if they are missing we return an error
+    # protocolKeywords = ['request', 'service', 'version', 'outputformat']
+    protocolKeywords = ['request', 'service', 'version']
+    if (request.args.get('request') and neutralCriteria['protocol']['request'] != 'getcapabilities' and neutralCriteria['protocol']['request'] != 'describefeaturetype'):
+      for pk in protocolKeywords:
+        if request.args.get(pk):
+          neutralCriteria['protocol'][pk.lower()] = request.args.get(pk).lower()
+        else:
+          neutralCriteria['protocol']['code'] = "MissingParameterValue"
+          neutralCriteria['protocol']['locator'] = pk
+          neutralCriteria['protocol']['error'] = "Request is missing required WFS parameter '" + pk + "'"
+  
+    criteriaKeywords = {'typeName':'featureType', 'featureID':'id', 'maxFeatures':'limit', 'sortBy':'sortby', 'propertyName':'columns', 'bbox':'boundingbox'}
+
+    for key, map in criteriaKeywords.items():
+      if request.args.get(key):
+        neutralCriteria['criteria'][map] = request.args.get(key).lower()
+  
+    # also add WKT from bounding box, if it exists
+    if request.args.get('bbox'):
+      extents = [x.strip for x in request.args.get('bbox').split(',')]
+      #neutralCriteria['criteria']['bboxWKT'] = "POLYGON ({a} {b}, {a} {d}, {c} {d}, {c} {b}, {a} {b})".format(a=extents[0], b=extents[1], c=extents[2], d=extents[3])
+
+    return neutralCriteria
+
+  def renderGetCapabilities(self, metadata):
+    return Response(render_template('wfs/getcapabilities.xml', metadata=metadata), mimetype='text/xml')
+
+  def renderDescribeFeatureType(self, metadata):
+    return Response(render_template('wfs/describefeaturetype.xml', metadata=metadata), mimetype='text/xml')
+
+  def renderGetFeature(self, metadata):
+    mime = 'text/xml'
+    layout = 'wfs/getfeature.xml'
+    if metadata['neutralsearch']['protocol'].get('outputformat'):
+      if re.match(r'.*json$', metadata['neutralsearch']['protocol'].get('outputformat').lower()):
+        #code=metadata['neutralsearch']['protocol']['outputformat']
+        #locator='WFS'
+        #error='Matched on matching *.json$'
+        #return render_template('wfs/error.xml', exceptioncode=code, locator=locator, error=error)
+        layout = 'wfs/getfeature.json'
+        mime = 'application/json;charset=UTF-8'
+  
+    return Response(render_template(layout, metadata=metadata), mimetype=mime)
+    # return Response(render_template(layout, metadata=metadata), mimetype=mime)
+  
+  def renderError(code, locator, error):
+    return render_template('wfs/error.xml', exceptioncode=code, locator=locator, error=error)
+  
+  def renderResponse(self, metadata):
+    requests = {}
+    requests['getcapabilities'] = self.renderGetCapabilities
+    requests['describefeaturetype'] = self.renderDescribeFeatureType
+    requests['getfeature'] = self.renderGetFeature
+
+    if metadata['neutralsearch']['protocol'].get('code'):
+      return renderError (metadata['neutralsearch']['protocol']['code'], metadata['neutralsearch']['protocol']['locator'], metadata['neutralsearch']['protocol']['error'])
+    # try:
+    wfsrequest = metadata['neutralsearch']['protocol']['request'].lower()
+    return requests[wfsrequest](metadata)
+    # except:
+    #   return renderError("OperationNotSupported", "request", "Unknown operation requested '" + wfsrequest + "'")
+  
+  def loadInterfaces(self):
+    interfaces = {}
+    interfaces['neutralSearch'] = self.neutralSearch
+    interfaces['renderResponse'] = self.renderResponse
+  
+    return interfaces
